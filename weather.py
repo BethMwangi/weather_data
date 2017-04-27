2a14e07099a952f97019b9d3254fd1c8aa42aa7a
BethMwangi <wanjikumwangib@gmail.com>
2017-04-27 14:40:41 +0300

    print biggest spread and day

--- weather.py
+++ weather.py
@@ -1,3 +1,7 @@
+# Initialize variables to hold day with biggest spread and biggest spread
+day_with_biggest_spread = None
+biggest_spread = None
+
 file_pointer = open('weather.dat', 'r')
 for line in file_pointer:
 	# print line
@@ -13,8 +17,26 @@ for line in file_pointer:
 	columns = [item for item in columns if  item != ""]
 	# print columns
 	day_of_month = columns[0].strip()
-	max_temp = columns[1].rstrip('*')
+	#remove the asterics bound with numbers in the respective columns
+	max_temp = columns[1].rstrip('*') 
 	min_temp = columns[2].rstrip('*')
 	# print day_of_month
 	# print max_temp
 	# print min_temp
+	#first line contains text elements, so go immediately to the next line
+	if day_of_month.isalpha():
+		continue
+	spread = int(max_temp) - int(min_temp)
+	# print spread
+
+	if day_with_biggest_spread is None and biggest_spread is None:
+		day_with_biggest_spread = day_of_month
+		biggest_spread = spread
+	else:
+		if spread > biggest_spread:
+			biggest_spread = spread
+			day_with_biggest_spread = day_of_month
+
+file_pointer.close()
+
+print day_with_biggest_spread, biggest_spread
