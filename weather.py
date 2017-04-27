9b6a7ed3abc19c26fa0f11ecdcaae58f6b8fec07
BethMwangi <wanjikumwangib@gmail.com>
2017-04-27 12:33:26 +0300

    print column items in lists

--- weather.py
+++ weather.py
@@ -6,4 +6,9 @@ for line in file_pointer:
 	# get rid of empty lines/rows
 	if line == '':
 		continue
-	print line
+	# print line
+	columns = line.split(" ")
+	#get rid of empty list elements created by non-uniform spaces
+	# in the columns
+	columns = [item for item in columns if  item != ""]
+	print columns
